ccbdd7911c4c79aa92b6ae08c74a05297dfe2156
Mercuriev, Igor <igor.mercuriev@thermofisher.com>
2020-05-29 19:07:22 +0300

    Added Module1.Cycles.Task1 and Module1.Lab1

---
+++ src/module1/cycles/PrimitiveTypes.java
@@ -0,0 +1,23 @@
+package module1.cycles;
+
+public class PrimitiveTypes {;
+    
+    public static void main(String[] args) {
+        byte age = 22;
+        System.out.println("My age is " + age + " years");
+        short weight = 78;
+        System.out.println("My weight is " + weight + " kg");
+        int height = 185;
+        System.out.println("My height is " + height + " cm");
+        long numberOfCells = 30;
+        System.out.println("My body has " + numberOfCells + " trillion cells");
+        float fiveDividedByTwo = 2.5f;
+        System.out.println("Five divided by two will be " + fiveDividedByTwo);
+        double pi = Math.PI;
+        System.out.println("PI number is " + pi);
+        boolean iAmAProgrammer = true;
+        System.out.println("An i a programmer? The answer is " + iAmAProgrammer);
+        char gender = 'm';
+        System.out.println("My gender is \'" + gender + "\'");
+    }
+}
